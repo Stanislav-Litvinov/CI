ba261b5b2940c074a1f5337ababbefab93759efc
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-12-18 16:04:01 +0300

    Added StatisticsServiceTest

--- src/main/java/ru/netology/LegacyCode.java
+++ src/main/java/ru/netology/LegacyCode.java
@@ -2,14 +2,10 @@ package ru.netology;
 
 public class LegacyCode {
 
-    public static void main(String[] args) {
-        class StatisticsService {
-            public long findMax(long[] incomes) {
-                long current_max_index = 0;
-                long current_max = incomes[0];
-                for (long income : incomes) if (current_max < income) current_max = income;
-                return current_max;
-            }
-        }
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes) if (current_max < income) current_max = income;
+        return current_max;
     }
 }
