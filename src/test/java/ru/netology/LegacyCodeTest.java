1430959fcd5b569734093c2ed284745f05022bd7
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-12-18 18:31:18 +0300

    100% coverage

--- src/test/java/ru/netology/LegacyCodeTest.java
+++ src/test/java/ru/netology/LegacyCodeTest.java
@@ -8,8 +8,8 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         LegacyCode service = new LegacyCode();
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 100, 8, 6, 11, 11, 12};
+        long expected = 100;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
