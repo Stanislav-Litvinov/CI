cad7778e1338807301b54370e4735becbbb3b19e
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-12-18 18:27:07 +0300

    Added check rules

--- src/test/java/ru/netology/LegacyCodeTest.java
+++ src/test/java/ru/netology/LegacyCodeTest.java
@@ -8,7 +8,7 @@ class StatisticsServiceTest {
     @Test
     void findMax() {
         LegacyCode service = new LegacyCode();
-        long[] incomesInBillions = {3, 5, 8, 4, 5, 12, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
